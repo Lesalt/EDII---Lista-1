0c3d13663f067d593e22e34347a16cccf9f5ed70
Lesalt <74870001+Lesalt@users.noreply.github.com>
2020-11-22 13:35:19 -0300

    Add files via upload

---
+++ Atividade_1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Atividade_1 {
+
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        String vetor[] = new String[5];
+        String nome;
+        String searchNome;
+        String resul = "";
+        for(int i = 0; i < vetor.length; i++){
+            System.out.println("Informe o " + (i + 1) + "º nome: ");
+            nome = read.nextLine();
+            vetor[i] = nome;
+        }
+        System.out.println("Informe o nome que deseja procurar: ");
+        searchNome = read.nextLine();
+        for(int j = 0; j < vetor.length; j++){
+            if(vetor[j].equals(searchNome)){
+                resul = String.format("O nome (" + searchNome + ") está na " + (j + 1) + "º posição do vetor.");
+                break;
+            }
+        }
+        if(resul != null){
+            System.out.println(resul);
+            System.out.println(vetor.length);
+        }else{
+            System.out.println("Nome não encontrado");
+        }
+    }
+}
